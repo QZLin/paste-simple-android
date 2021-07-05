dc403fd64909cf84339e7abaccd2e11588a240c4
QZLin <qzlin01@163.com>
2021-07-06 00:06:07 +0800

    update license of ClipBoardMonitor

--- app/src/main/java/com/pishangujeniya/clipsync/service/ClipBoardMonitor.kt
+++ app/src/main/java/com/pishangujeniya/clipsync/service/ClipBoardMonitor.kt
@@ -15,22 +15,12 @@ import java.net.URLEncoder
 import java.util.*
 import java.util.concurrent.Executors
 
+
 /*
-* Copyright 2013 Tristan Waddington
-*
-*    Licensed under the Apache License, Version 2.0 (the "License");
-*    you may not use this file except in compliance with the License.
-*    You may obtain a copy of the License at
-*
-*        http://www.apache.org/licenses/LICENSE-2.0
-*
-*    Unless required by applicable law or agreed to in writing, software
-*    distributed under the License is distributed on an "AS IS" BASIS,
-*    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*    See the License for the specific language governing permissions and
-*    limitations under the License.
+*   Origin file information: Copyright 2013 Tristan Waddington, under the Apache License, Version 2.0
+*   Full LICENSE information of this file: LICENSE_ClipBoardMonitor.txt
+*   This file is already modified
 */
-//Thanks to the code author
 /**
  * Monitors the [ClipboardManager] for changes and logs the text to a file.
  */
