7d446b8b5c6864bdc3120faf1e1a61f2509e94a5
QZLin <qzlin01@163.com>
2021-07-06 17:37:44 +0800

    1.1-Beta

--- app/src/main/java/com/qzlin/pastesimple/Global.kt
+++ app/src/main/java/com/qzlin/pastesimple/Global.kt
@@ -4,12 +4,13 @@ object Global {
     const val SignalHubName = "SignalRHub"
     const val receive_copied_text_signalr_method_name = "ReceiveCopiedText"
     const val send_copied_text_signalr_method_name = "SendCopiedText"
-    val copied_water_mark = "- Copied By ClipSync"
+
+    //    val copied_water_mark = "- Copied By ClipSync"
     const val STOP_SERVICE = "STOP_SERVICE"
     const val START_SERVICE = "START_SERVICE"
     const val SIGNALR_SERVICE_NOTIFICATION_ID = 1001
 
-    const val STATUS_UPDATE_ACTION = "com.qzlin.pastesimple.action"
+    const val STATUS_UPDATE_ACTION = "com.qzlin.pastesimple.update_status"
 
     var lastSetText = ""
     var waitCopyLoop = false
